2261d4018b4cf0ec8f5ffa6c9400e832221b7ac3
marcial melong <s1270701@student.mcckc.edu>
2020-10-12 12:53:44 -0500

    add my new commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import ExpenseList from "./components/ExpenseList";
+import ExpenseForm from "./components/ExpenseForm";
+import Alert from "./components/Alert";
+import { v4 as uuid } from "uuid";
+
+const initialExpenses = [
+  { id: uuid(), charge: "rent", amount: 1000 },
+  { id: uuid(), charge: "car payment", amount: 300 },
+  { id: uuid(), charge: "credit card bill", amount: 1400 },
+];
+
+console.log(initialExpenses);
+//import useState()
+//function return [] with two values
+//the actual value of the state
+//function for updates/control
+//default value
 
 function App() {
+  // ****** state values****
+  // all expenses, add expense
+  const [expenses, setExpenses] = useState(initialExpenses);
+  // single expense
+  const [charge, setCharge] = useState("");
+  //sinle amount
+  const [amount, setAmount] = useState("");
+
+  // **** funtionality****
+  const handleCharge = (e) => {
+    setCharge(e.target.value);
+  };
+  const handleAmount = (e) => {
+    setAmount(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Alert />
+      <h1>budget calculator</h1>
+      <main className="App">
+        <ExpenseForm
+          charge={charge}
+          amount={amount}
+          handleAmount={handleAmount}
+          handleCharge={handleCharge}
+          handleSubmit={handleSubmit}
+        />
+        <ExpenseList expenses={expenses} />
+      </main>
+      <h1>
+        total spending :{" "}
+        <span className="total">
+          ${" "}
+          {expenses.reduce((a, c) => {
+            return (a += c.amount);
+          }, 0)}{" "}
+        </span>
+      </h1>
+    </>
   );
 }
 
